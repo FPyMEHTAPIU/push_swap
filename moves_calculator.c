50fbbdb327a535b245b026c233558e8908bb0a8d
Nick Saveliev <kolya59264@gmail.com>
2024-06-25 13:55:51 +0300

    Implement stack copying and fetching the lowest number of movements

--- moves_calculator.c
+++ moves_calculator.c
@@ -6,20 +6,37 @@
 /*   By: msavelie <msavelie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 16:10:39 by msavelie          #+#    #+#             */
-/*   Updated: 2024/06/24 17:21:33 by msavelie         ###   ########.fr       */
+/*   Updated: 2024/06/25 13:54:50 by msavelie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+// This function just copies a stack to the temp one for the further calculations
+static int	*copy_stack(int *stack, int size)
+{
+	int	*temp;
+	int	i;
 
-//TODO:
-	//Create a copy array for calculations (without allocation)
+	i = 0;
+	temp = malloc(sizeof(int) * size);
+	if (!temp)
+		return (NULL);
+	while (i < size)
+	{
+		temp[i] = stack[i];
+		i++;
+	}
+	return (temp);
+}
+
+// This function compares a number from stack with all numbers in another stack
 static int	compare_num_with_stack(int num, int *b, int size_b)
 {
 	int	j;
 	int	max;
 	int	index;
+	int	*temp_stack;
 
 	j = size_b - 1;
 	max = b[j];
@@ -33,21 +50,60 @@ static int	compare_num_with_stack(int num, int *b, int size_b)
 		j--;
 	}
 	j = 1;
-	while (max != b[size_b - 1])
+	temp_stack = copy_stack(b, size_b);
+	while (max != temp_stack[size_b - 1])
 	{
 		if (index > (size_b - 1) / 2)
-			rotate_one(b, size_b, 'b');
+		{
+			rotate_one(temp_stack, size_b, 0);
+			j++;
+		}
 		else
-			rrotate_one(b, size_b, 'b');
-		j++;
+		{
+			rrotate_one(temp_stack, size_b, 0);
+			j--;
+		}
 	}
+	free(temp_stack);
 	return (j);
 }
 
+//This function returns the index of element with the cheapest movement 
+static int	find_min(int *indexes, int size)
+{
+	int	i;
+	int	min_pos;
+	int	min_neg;
+
+	i = 0;
+	min_pos = 0;
+	min_neg = 0;
+	while (i < size)
+	{
+		if (indexes[i] < 0)
+		{
+			if (min_neg > indexes[i])
+				min_neg = indexes[i];
+		}
+		else
+		{
+			if (min_pos < indexes[i])
+				min_pos = indexes[i];
+		}
+		i++;
+	}
+	if ((min_neg * -1) < min_pos)
+		return (min_neg);
+	else
+		return (min_pos);
+}
+
+// This function calculates all moves in current situation and returns the lowest value
 int	calculator(int *a, int *b, int size_a, int size_b)
 {
 	int	indexes[size_a];
 	int	i;
+	int	min_moves_ind;
 
 	i = size_a - 1;
 	while (i >= 0)
@@ -56,4 +112,7 @@ int	calculator(int *a, int *b, int size_a, int size_b)
 		indexes[i] = compare_num_with_stack(a[i], b, size_b);
 		i--;
 	}
+	// Find minimum number in array
+	min_moves_ind = find_min(indexes, size_a);
+	return (min_moves_ind);
 }
