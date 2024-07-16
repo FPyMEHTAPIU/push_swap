1e87f4ee05bb8815d5657d460422698f66ecf1ae
msavelie <msavelie@student.hive.fi>
2024-07-16 13:44:07 +0300

    Fix ft_atoi for the INT_MIN and INT_MAX

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,15 +6,37 @@
 /*   By: msavelie <msavelie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 16:00:25 by msavelie          #+#    #+#             */
-/*   Updated: 2024/06/11 17:20:28 by msavelie         ###   ########.fr       */
+/*   Updated: 2024/07/16 13:43:02 by msavelie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+static int	check_range(int num, int sign, const char *str)
+{
+	long long	check;
+
+	while ((*str >= '0' && *str <= '9') && *str)
+	{
+		check = num * 10 + *str - '0';
+		if ((check == -2147483648 || check == 2147483648) && sign == -1)
+			return (-2147483648);
+		if (check == 2147483647 && sign == 1)
+			return (2147483647);
+		if (check / 10 != num && sign == 1)
+			return (-1);
+		if (check / 10 != num && sign == -1)
+			return (0);
+		num = check;
+		str++;
+	}
+	return ((num * sign));
+}
 
 int	ft_atoi(const char *str)
 {
 	int			sign;
 	int			num;
-	long long	check;
 
 	num = 0;
 	sign = 1;
@@ -26,15 +48,5 @@ int	ft_atoi(const char *str)
 			sign *= -1;
 		str++;
 	}
-	while ((*str >= '0' && *str <= '9') && *str)
-	{
-		check = num * 10 + *str - '0';
-		if (check / 10 != num && sign == 1)
-			return (-1);
-		if (check / 10 != num && sign == -1)
-			return (0);
-		num = check;
-		str++;
-	}
-	return ((num * sign));
+	return (check_range(num, sign, str));
 }
